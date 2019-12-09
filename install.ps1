395f01f2e3481c0f9e75f5d01f598d8eeb258735
Martin Brandl <mjisaak@users.noreply.github.com>
2019-12-09 17:12:12 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 param(
   $title
 )
-
-$newTitle = $title -replace '.*to: '
+write-host "current title: $title"
+$newTitle = "$title" -replace '.*to: '
 Write-Host "new title: $newTitle"
 az webapp config appsettings set -g github-action-rg -n github-action-demo --settings title=$newTitle
