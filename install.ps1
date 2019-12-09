e6afe53b3507b1d0dc9b4b1b2f65a2f28d680bd6
Martin Brandl <mjisaak@users.noreply.github.com>
2019-12-09 17:03:21 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,3 @@
-$newTitle = '${{ github.event.issue.title }}' -replace '.*to: '
+$newTitle = '$env:github.event.issue.title' -replace '.*to: '
+Write-Host "new title: $newTitle"
 az webapp config appsettings set -g github-action-rg -n github-action-demo --settings title=$newTitle
