68f387c594367668e6183d6360125f93136b69cd
Martin Brandl <mjisaak@users.noreply.github.com>
2019-12-12 13:31:10 +0100

    Delete install.ps1

--- install.ps1
+++
@@ -1,7 +0,0 @@
-param(
-  $title
-)
-write-host "current title: $title"
-$newTitle = "$title" -replace '.*to: '
-Write-Host "new title: $newTitle"
-az webapp config appsettings set -g github-action-rg -n github-action-demo --settings title=$newTitle
